b5d9b8dade739e3176faa182ef41402b8a598c23
Max Rose-Innes <59234469+maxroseinnes@users.noreply.github.com>
2022-10-15 18:09:21 -0700

    oops

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const port = 3000;
 app.use(express.static("public"));
 
 socketServer.on("connection", (socket) => {
-  console.log("New client connected!");e
+  console.log("New client connected!");
+  socket.on("message", (data) => {
+    console.log("Message: " + data);
+    socket.broadcast.emit("message", data);
+  })
 });
 
 server.listen(port, () => {
